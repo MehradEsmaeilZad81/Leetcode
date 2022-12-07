503aacf5c0e738553ba46cea1be77164ec420fa1
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-07 13:43:42 +0330

    Problem Number #1 "O(n)"

--- Problems/1. Two Sum.py
+++ Problems/1. Two Sum.py
@@ -3,12 +3,12 @@ target = int(input())
 
 
 def twoSum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    
-    return None
+    tmp = {}
+    for i, num in enumerate(nums):
+        if target - num in tmp:
+            return [tmp[target - num], i]
+        tmp[num] = i
+    return [-1, -1]
 
 
 print(twoSum(nums, target))
